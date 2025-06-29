3547c0a142d00e301c2853d12a816aad57d11b62
Taseen Jeem <taseenjeem.dev@gmail.com>
2025-06-29 22:30:23 +0600

    Added data to the database

---
+++ app/api/products/route.ts
@@ -0,0 +1,15 @@
+import database from "@/lib/db";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  try {
+    const data = await database.products.findMany();
+    return NextResponse.json(data, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch products data" },
+      { status: 500 }
+    );
+  }
+}
