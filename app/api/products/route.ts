571d4e65138c02880ea175b254d32416067818ab
Taseen Jeem <taseenjeem.dev@gmail.com>
2025-06-30 17:08:39 +0600

    updated api and schema

--- app/api/products/route.ts
+++ app/api/products/route.ts
@@ -3,7 +3,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const data = await database.products.findMany();
+    const data = await database.products.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
     return NextResponse.json(data, { status: 200 });
   } catch (error) {
     console.error("Error fetching products:", error);
